6523c6932afd4d2c0188d4c262c8614e60fda3a6
vinceeverton <75642818+vinceeverton@users.noreply.github.com>
2026-02-04 18:14:34 -0500

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://bulliq:bulliq@db:5432/bulliq"
+DATABASE_URL = "sqlite:///bulliq.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
 SessionLocal = sessionmaker(bind=engine)
