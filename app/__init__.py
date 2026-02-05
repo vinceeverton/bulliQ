bf2cdead679f3ba88ddd2e02db0180d8ce09f66d
Everton Vince <evertonipm@icloud.com>
2026-02-05 18:22:58 -0500

    d

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-
+import os 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates', static_folder='static')
 
     # Instead of blueprints, import routes directly
     from .routes import main
