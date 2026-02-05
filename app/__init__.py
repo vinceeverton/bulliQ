24a75b23ef21f103e6e5f477be77b951101c5f91
vinceeverton <75642818+vinceeverton@users.noreply.github.com>
2026-02-05 16:29:36 -0500

    Refactor app initialization and register main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,16 @@
-# required for package recognition
-
 from flask import Flask
 
 def create_app():
     app = Flask(__name__)
 
     from .routes import main
-    from .camera import camera_bp 
+    from .camera import camera_bp
     from .calibration import calib_bp
     from .dashboard import dashboard_bp
-    
-app.register_blueprint(camera_bp, url_prefix='/camera')
-app.register_blueprint(calib_bp, url_prefix='/calibration')
-app.register_blueprint(dashboard_bp, url_prefix='/dashboard')
 
-return app
+    app.register_blueprint(main)
+    app.register_blueprint(camera_bp, url_prefix="/camera")
+    app.register_blueprint(calib_bp, url_prefix="/calibration")
+    app.register_blueprint(dashboard_bp, url_prefix="/dashboard")
+
+    return app
