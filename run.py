381a6d4089926542b97ab2c92dbff4287b489658
Everton Vince <evertonipm@icloud.com>
2026-02-05 17:09:40 -0500

    modified: runpy

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
-from app import create_app
-app = create_app()
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7000, debug=True)
+from flask import Flask
+
+def create_app():
+    # explicitly set template folder
+    app = Flask(__name__, template_folder="../templates")
+
+    from .routes import main_bp
+    app.register_blueprint(main_bp)
+
+    return app
