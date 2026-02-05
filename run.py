a23ec91fde4d0965f44c7a127316010e96341a4b
Everton Vince <evertonipm@icloud.com>
2026-02-05 17:19:27 -0500

    update

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 from flask import Flask
-
+from app import create_app
+app = create_app()
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7000, debug=True)
+    
 def create_app():
     # explicitly set template folder
     app = Flask(__name__, template_folder="../templates")
