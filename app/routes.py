2ba181165f8a616a1d6ff76dafa7c89910dab668
Everton Vince <evertonipm@icloud.com>
2026-02-05 19:47:48 -0500

    edit 5

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
-main_bp = Blueprint("main", __name__)
+routes_bp = Blueprint('routes', __name__)
 
-@main_bp.route('/')
+@routes_bp.route('/')
 def index():
     return render_template("index.html")
