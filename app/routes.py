7609f4ac9a9399b7abfbf967141088b21353a01b
Everton Vince <evertonipm@icloud.com>
2026-02-05 20:27:51 -0500

    edites

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
-routes_bp = Blueprint('routes', __name__)
+main_bp = Blueprint('main', __name__)
 
-@routes_bp.route('/')
+@main_bp.route('/')
 def index():
     return render_template("index.html")
