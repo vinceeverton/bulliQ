24568e4e73a15d45941b75fe602bfb5d0d5519cb
Everton Vince <evertonipm@icloud.com>
2026-02-05 20:35:03 -0500

    g

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ def create_app():
     app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
     # Import and register all blueprints
-    from .routes import routes
+   # from .routes import routes
     from .camera import camera_bp
     from .calibration import calib_bp
     from .dashboard import dashboard_bp
