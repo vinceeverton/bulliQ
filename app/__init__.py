6136c64bab118716d6294754be5cbd94071adc74
Everton Vince <evertonipm@icloud.com>
2026-02-05 20:32:19 -0500

    d

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
+from pdb import main
 from flask import Flask
 
 def create_app():
     app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
     # Import and register all blueprints
-    from .routes import routes_bp as main
+    from .routes import routes
     from .camera import camera_bp
     from .calibration import calib_bp
     from .dashboard import dashboard_bp
 
-    app.register_blueprint(main)
+    app.register_blueprint(routes)
     app.register_blueprint(camera_bp, url_prefix='/camera')
     app.register_blueprint(calib_bp, url_prefix='/calibration')
     app.register_blueprint(dashboard_bp, url_prefix='/dashboard')
